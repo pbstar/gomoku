40e4544173b35322ee86948bbf82490eb37e620e
huangzq <13402318457@163.com>
2024-08-31 15:46:22 +0800

    refactor: perfect home.vue

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="home">
-    <h1>五子棋</h1>
-    <p>游戏规则：黑白两方轮流下子，先连成五个子的一方获胜。</p>
-    <RouterLink to="/play">
-      <button>开始游戏</button>
-    </RouterLink>
-    <RouterLink to="/playAi">
-      <button>人机对决</button>
-    </RouterLink>
+  <div class="game-home">
+    <div class="game-title">五子棋</div>
+
+    <p class="game-desc">
+      游戏规则：黑白两方轮流下子，先连成五个子的一方获胜。
+    </p>
+
+    <div class="game-play">
+      <RouterLink to="/play">
+        <button>开始游戏</button>
+      </RouterLink>
+      <RouterLink to="/playAi">
+        <button>人机对决</button>
+      </RouterLink>
+    </div>
   </div>
 </template>
 
@@ -16,26 +22,30 @@ import { RouterLink } from "vue-router";
 </script>
 
 <style scoped>
-.home {
+.game-home {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100vh;
 }
-h1 {
+
+.game-title {
   font-size: 30px;
   text-align: center;
   margin-top: 50px;
 }
-p {
+
+.game-desc {
   font-size: 20px;
   text-align: center;
   margin-top: 20px;
   margin-bottom: 20px;
 }
 
-button {
-  margin-top: 20px;
+.game-play {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 }
 </style>
